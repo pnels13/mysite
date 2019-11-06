c004376ec056c7a7201cf8cebdc20d6315486334
Preston Nelson <pnels13@lsu.edu>
2019-11-05 22:14:02 -0600

    configuring for heroku

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\17716\onedrive\docume~1\isds31~1\projects\mysite\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
